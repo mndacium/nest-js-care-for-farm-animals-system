cca9266ef759a8249e483dc6c493a3e04c8de206
mndacium <vladtanasiichuk@gmail.com>
2024-06-08 20:53:02 +0300

    github action ci workflow

---
+++ src/hello-world.spec.ts
@@ -0,0 +1,9 @@
+describe('Mock test', () => {
+  describe('helloWorld', () => {
+    it('should be "Hello World!"', () => {
+      const helloWorld = 'Hello World!';
+
+      expect(helloWorld).toBe('Hello World!');
+    });
+  });
+});
